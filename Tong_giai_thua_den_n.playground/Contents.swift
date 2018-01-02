dc4abf97f0cb6de5fb642a72b0aa7e398e59b0bd
MauBao <ngomaubaodhcn1b@gmail.com>
2018-01-02 10:40:23 +0700

    Tính tổng giai thừa

---
+++ Tong_giai_thua_den_n.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var n: Int = 4
+var s: Int = 1
+var tong : Int = 0
+for i in 1...n {
+    s = s * i
+    tong = tong + s
+}
+print(tong)
+
