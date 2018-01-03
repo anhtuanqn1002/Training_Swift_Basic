0d8376d6784520cdb656169745b2713b6172fd72
MauBao <ngomaubaodhcn1b@gmail.com>
2018-01-03 19:33:22 +0700

    Thêm một phần tử cho mảng

---
+++ Them_1_phan_tu_cho_mang.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//var arr: [Int] = [1,2]
+//var i: Int = 3
+//arr.append(i)
+//    print(arr)
+
+//Thêm trực tiếp
+var arr: [Int] = [1,2]
+arr += [4]
+print(arr)
