da2425a6c85e4884e0204798c0bb805c6c1281a6
MauBao <ngomaubaodhcn1b@gmail.com>
2018-01-03 20:01:07 +0700

    Số nguyên tố chỉnh sửa 1

--- So_nguyen_to.playground/Contents.swift
+++ So_nguyen_to.playground/Contents.swift
@@ -1,28 +1,25 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
-var n: Int = 2
+var n: Int = 11
 let m: Int
-var bd: Int = 0
+//var bd: Int = 0
 
-if n < 2 {
-    print("Bạn nhập sai rồi")
-} else {
-m = n / 2
-for i in 1...m {
-    if n % i == 0  {
-        print("Số vừa nhập không phải là số nguyên tố")
-        bd = 1;
-        break
-}
-}
-if bd == 0 {
+if n == 3 {
     print("Số vừa nhập là số nguyên tố")
 }
+
+if n == 2 {
+    print("Số vừa nhập là số nguyên tố")
+} else {
+    m = n / 2
+    for i in 2...m {
+        if n % i == 0  {
+            print("Số vừa nhập không phải là số nguyên tố")
+            break
+        } else {
+            //bd == 0
+            print("Số vừa nhập là số nguyên tố")
+            break
+        }
+    }
 }
-
-
-
-
-
