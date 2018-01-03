10ff06dc54a54b42a27a0ebcfda38864f85ded4f
MauBao <ngomaubaodhcn1b@gmail.com>
2018-01-03 19:31:45 +0700

    Tìm số có giá trị nhỏ nhất của một mảng

---
+++ Tim_so_nho_nhat_mang.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var arr = [2,4,6,7,-1]
+var min = arr[0]
+for i in arr {
+    if min > i {
+      min = i
+      
+   }
+}
+print(min)
+
+
+
+
